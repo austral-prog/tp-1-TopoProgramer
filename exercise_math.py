779b39a448a2d3b0f1149a19644723e685101bd6
Bruno <116372845+TopoProgramer@users.noreply.github.com>
2024-03-15 11:58:41 -0300

    Bruno Almiron Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,11 +1,19 @@
 def math():
     a = 57
     b = 7
+
+    Suma = a + b
+    Diferencia = a - b
+    Producto = a * b
+    Promedio = (a + b) / 2
+    Cociente_e = a // b
+    Resto_e = a % b
+    Valor_r = a / b
     
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print((a + b) / 2)
-    print(a // b)
-    print(a % b)
-    print(a / b)
+    print(Suma)
+    print(Diferencia)
+    print(Producto)
+    print(Promedio)
+    print(Cociente_e)
+    print(Resto_e)
+    print(Valor_r)
