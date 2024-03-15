1fceada62465787ba5697d14d028396ca166764e
Bruno <116372845+TopoProgramer@users.noreply.github.com>
2024-03-15 11:36:40 -0300

    Bruno Almiron Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+"""la suma
+la diferencia
+el producto
+el promedio
+el cociente entero
+el resto de la división entera 
+el valor real de la división """
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print((a + b) / 2)
+    print(a % b)
+    print(a // b)
+    print(a / b)
